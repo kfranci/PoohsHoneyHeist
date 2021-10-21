96147f7e72a83d6e8f6c821801e4af4a6eb42042
JoshHatchet <joshbordick@gmail.com>
2021-10-21 12:17:07 -0600

    added comments to code and added a readme

---
+++ README.txt
@@ -0,0 +1,11 @@
+Joshua Bordick, Kylee Franci, David Shearon, Anna Otterstetter
+
+Justfying 100 Points
+
+The gameplay is very dynamic and includes the use of collison detection with different objects such as the bees and the honey that pooh
+throws as well as the interation with pooh and the bees. There is a life system as well as a score system whcih works with collision detection.
+There are also plenty of stylign features that are dynamic and change with gameplay state. This can be seen in the endgame function as well as
+the dispStats function. The features that were a bear to program were the collison detection as well as the cooldown for the space bar since 
+they had to go through thorough testing and were bugging when the code was being developed. The setInterval functions were also difficult as they 
+would not work as intened for a very long time. 
+
--- pooh.js
+++ pooh.js
@@ -1,60 +1,96 @@
+// Joshua Bordick, Kylee Franci, David Shearon, Anna Otterstetter
+
+// Javascript Project
+
+
+
+// variables used for gameplay and setup
 var width = 800;
 var height = 600;
 var xPos=50;
 var yPos=240;
-var playerSize=75;
-var playerSpeed=15;
+var playerSize=150;
+var playerSpeed=20;
 var bees=[];
-var beeSize=50;
-var beeSpeed=10;
+var beeSize=60;
+var beeSpeed=8;
 var honey=[];
 var shotSize=30;
 var shotSpeed=10;
+
+// setup image variables
 var img = new Image();
 var img2 = new Image();
 var img3 = new Image();
 var img4 = new Image();
-var time = 1500;
-var updateTime = 1000/20;
-var bufferTop = playerSize/2; 
-var bufferBottom = height-playerSize/2;
 img.src = 'images/pooh.png';
 img2.src = 'images/bees.png';
 img3.src = 'images/honey.png';
 img4.src = 'images/back.jpg';
+
+
+// setup time and buffer variables
+var time = 1500;
+var updateTime = 1000/20;
+var bufferTop = 75/2; 
+var bufferBottom = height-75/2;
+
+// score and lives variables for game
 var lives = 3;
 var score = 0;
+
+//button press codes
+var spaceCooldown = 0;
 var space = 32;
 var up = 38;
 var down = 40;
 var timeoutTime = 5000;
+
+// setup timing variables for gameplay
 var initOne = setInterval(update,updateTime);
 var initTwo = setInterval(createBee,time);   
 document.addEventListener('keydown',keyPush);
+document.addEventListener('keyup', refreshSpace);
 
-
+// get the elements that will be needed for the game
 function init() {
 
 	canvas=document.getElementById("canvas");
 	context=canvas.getContext("2d");
 }
 
-
+// run init onload
 window.onload = init();
 
+// create bees when prompted
 function createBee() {
-	bees.push({x:canvas.width,y:Math.random()*canvas.height});
+	bees.push({x:canvas.width,y:Math.random()*(bufferBottom - bufferTop) + bufferTop});
 }
-function update() {
+
+// show the score and lives when there is an update needed
+function dispStats(){
 	context.drawImage(img4, 0,0,canvas.width,canvas.height);
-	context.fillStyle = "purple";
-	context.font = "30px Arial";
+	context.fillStyle = "white";
+	context.font = "bold 30px Arial";
 	context.fillText("Lives: " + lives, width - bufferTop*5, bufferBottom);
 	context.fillText("Score: " + score, width-bufferTop*5, bufferTop);
+}
+
+
+// this is the ain logic loop og the game and handles the gameplay
+function update() {
+	dispStats();
+
+	// draw pooh image to board
 	context.drawImage(img,xPos-playerSize/2,yPos-playerSize/2,playerSize,playerSize);
+
+	// move the honey across the screen when needed
 	for(var i=0;i<honey.length;i++) {
 		honey[i].x += shotSpeed;
+		// draw honey
 		context.drawImage(img3, honey[i].x-shotSize/2,honey[i].y-shotSize/2,shotSize,shotSize);
+
+		// this loop is used to check for collisions when bees and honey interact
 		for(var j=bees.length-1;j>=0;j--) {
 			var dx=Math.abs(bees[j].x-honey[i].x);
 			var dy=Math.abs(bees[j].y-honey[i].y);
@@ -65,19 +101,24 @@ function update() {
 				score += 10;
 			}
 		}
-	}    
+	}   
+
+	// this loop here checks for if a bee hits a player  
 	for(var k=0;k<bees.length;k++) {
 		bees[k].x -= beeSpeed;
+		// draw bee
 		context.drawImage(img2, bees[k].x-beeSize/2,bees[k].y-beeSize/2,beeSize,beeSize);
 		var dx=Math.abs(bees[k].x-xPos);
 		var dy=Math.abs(bees[k].y-yPos);
 		var dist=Math.sqrt(dx*dx+dy*dy);
+		// check for collision
 		if(dist < (playerSize+beeSize)/2) {
 			honey=[];
 			bees=[];
 			xPos = 50;
 			yPos = 240;
 			lives--;
+			// check to see if player has no more lives
 			if(lives == 0){
 				endGame();
 				clear();
@@ -85,23 +126,43 @@ function update() {
 			}
 			break;
 		}
+		// subtarct score if bee gets passed pooh
+		else if( bees[k].x == 0 ){
+			score -= 10;
+			if(score < 0){
+				honey=[];
+				bees=[];
+				xPos = 50;
+				yPos = 240;
+				lives--;
+				score = 0;
+				if(lives == 0){
+					endGame();
+					clear();
+					break;
+				}
+			}
+		}
 	}
 }
 
-
+// logic for key presses and what shoulf happen
 function keyPush(e) {
 	switch(e.keyCode) {
 		case space:
-			honey.push({x:xPos,y:yPos});
+			if(spaceCooldown == 0){
+				honey.push({x:xPos,y:yPos});
+				spaceCooldown = 1;
+			}
 			break;
 		case up:
-			if(yPos-playerSpeed <= bufferTop){
+			if(yPos-playerSpeed <= 75/2 -15){
 				break;
 			}
 			yPos-=playerSpeed;
 			break;
 		case down:
-			if(yPos+playerSpeed >= bufferBottom){
+			if(yPos+playerSpeed >= height - playerSize/2+10){
 				break;
 			}
 			yPos+=playerSpeed;
@@ -109,18 +170,24 @@ function keyPush(e) {
 	}
 }
 
+// end intervals for game
 function clear() {
 	clearInterval(initOne);
 	clearInterval(initTwo);
 }
 
+// change the game state to show that the game is over and display stats
 function endGame(){
+	dispStats();
 	canvas=document.getElementById("canvas");
 	context=canvas.getContext("2d");
 	context.drawImage(img4, 0,0, canvas.wdith, canvas.height);
-	context.fillStyle = "purple";
-	context.font = "30px Arial";
-	context.fillText("No Lives Left! You Lose! Refresh to play again!", width/2 - bufferTop*8, height/2);  
+	context.fillStyle = "black";
+	context.font = "bold 30px Arial";
+	context.fillText("Ran Out of Lives- Refresh to Play Again!", width/2 - bufferTop*8, height/2);  
 }
 
-	
+// cooldown so space cannot be held down
+function refreshSpace(){
+	spaceCooldown = 0;
+}
